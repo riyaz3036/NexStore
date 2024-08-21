4ac79d503ecaaf6e9e17b254ec461b94d029834f
Riyaz Ahmed <riyaz.mittu@gmail.com>
2024-08-22 00:03:48 +0530

    fixed image issue by adding domain in config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 // next.config.mjs
 export default {
     images: {
-      domains: ['localhost','nexstore-nez2.onrender.com'],
+      domains: ['localhost','nexstore-hcw6.onrender.com'],
     },
   };
   
